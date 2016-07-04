ab54693ac7eb4aa45fb6b71ddff25553b1317604
hata <hata@stylerecipe.co.jp>
2016-07-04 22:39:51 +0900

    Add "import" extraction function.

--- clojure_inline_namespace_refactoring.py
+++ clojure_inline_namespace_refactoring.py
@@ -3,81 +3,126 @@ import re
 
 class ClojureInlineNamespaceRefactoringCommand(sublime_plugin.TextCommand):
 
-	__clj_ns_char_regex = re.compile(r"[-.\w\d]")
-
-	def extract_namespace_phrase_region_at(self, pos):
-		_pos = (pos - 1) if self.view.substr(pos) == "/" else pos
-		begin, end = _pos, _pos
-		while True:
-			if self.__class__.__clj_ns_char_regex.match(self.view.substr(begin)) == None:
-				break
-			begin -= 1
-		while True:
-			if self.__class__.__clj_ns_char_regex.match(self.view.substr(end)) == None:
-				break
-			end += 1
-		return sublime.Region(begin + 1, end)
-
+	__ns_class = sublime.CLASS_WORD_START | sublime.CLASS_WORD_END
 
 	def run(self, edit):
 		print("ClojureNsExtractor called")
 		v = self.view
-		sel = self.view.sel()
-		alias_text_regions = []
 
-		# 1. get namespace text
-		ns_region = self.extract_namespace_phrase_region_at(sel[0].begin())
-		ns = v.substr(ns_region).strip().split("/")[0]
-		if len(ns) == 0:
+		# get namespace text
+		inline_region = self.find_ns_modification(self.view.sel()[0].begin())
+		print("[inline phrase = {0}]".format(v.substr(inline_region)))
+		if inline_region.begin() == inline_region.end():
 			sublime.error_message("No namespace phrase at current cursol position.")
 			return
-		ns_suffix = ns.split(".")[-1]
-		ns_alias = ns_suffix
-		print("[ns, suffix, alias = {0}, {1}, {2}]".format(ns, ns_suffix, ns_alias))
 
-		# 2. insert "require" phrase in ns
+		if v.substr(inline_region.end()) == "/":
+			self.extract_inline_ns_modification(edit, inline_region)
+		else:
+			self.extract_inline_pkg_modification(edit, inline_region)
+		print("ClojureNsExtractor finished")
+
+	def find_ns_modification(self, pos):
+		v = self.view
+		r = v.expand_by_class(pos, self.__class__.__ns_class, " /({[")
+		return v.find(r"[-\w\d.]+[-\w\d]", r.begin())
+
+	def extract_inline_pkg_modification(self, edit, inline_region):
+		v = self.view
+
+		fqcn = v.substr(inline_region)
+		parts = fqcn.split(".")
+		if len(parts) < 2:
+			return
+		pkgname = ".".join(parts[:-1])
+		classname = parts[-1]
+		print("[pkgname, classname = {0}, {1}]".format(pkgname, classname))
+
+		# 2. insert "import" phrase in ns form
+		ns_and_the_1st_arg_region = v.find(r"\(ns[,\s]+[^,\s]+", 0)
+		import_region = v.find(r"\(ns[,\s](.|\n)+\(:import[\s,][^)]+?\)", 0)
+		print("import code region: " + str(import_region))
+		if ns_and_the_1st_arg_region.end() < 0:
+			# 2-1. There id no ns form
+			end_of_shebang_or_dependencies_regions = v.find_all(r"^(#!.+|'\{:dependencies(.|\n)+?\})")
+			if len(end_of_shebang_or_dependencies_regions) > 0:
+				# 2-1-1. There are shebang or dependencies
+				import_code = "\n(import '({0} {1}))".format(pkgname, classname)
+				import_code_insertion_pos = end_of_shebang_or_dependencies_regions[-1].end()
+			else:
+				# 2-1-2. There are not both shebang and dependencies
+				import_code = "(import '({0} {1}))\n".format(pkgname, classname)
+				import_code_insertion_pos = 0
+			v.insert(edit, import_code_insertion_pos, import_code)
+			added_import_alias_end_pos = import_code_insertion_pos + len(import_code) - 3
+		elif import_region.end() < 0:
+			# 2-2. There is no ":import" in ns form
+			import_code = "\n  (:import ({0} {1}))".format(pkgname, classname)
+			import_code_insertion_pos = ns_and_the_1st_arg_region.end()
+			v.insert(edit, import_code_insertion_pos, import_code)
+			added_import_alias_end_pos = import_code_insertion_pos + len(import_code) - 2
+		else:
+			# 2-3. There is ":import" in ns form
+			import_code = "\n           ({0} {1})".format(pkgname, classname)
+			import_code_insertion_pos = import_region.end() - 1
+			v.insert(edit, import_code_insertion_pos, import_code)
+			added_import_alias_end_pos = import_code_insertion_pos + len(import_code) - 1
+
+		# replace all of the inlines
+		fqcn_regexp = fqcn.replace(".", "\.") # TODO: general and safe way to `string -> regexp` conversion
+		for i in range(len(v.find_all(fqcn_regexp))):
+			fqcn_region = v.find(fqcn_regexp, 0)
+			v.replace(edit, fqcn_region, classname)
+
+	def extract_inline_ns_modification(self, edit, inline_region):
+		v = self.view
+
+		replaced_regions = []
+		ns = v.substr(inline_region)
+		alias = ns.split(".")[-1]
+		print("[ns, alias = {0}, {1}]".format(ns, alias))
+
+		# 2. insert "require" phrase in ns form
 		ns_and_the_1st_arg_region = v.find(r"\(ns[,\s]+[^,\s]+", 0)
 		require_region = v.find(r"\(ns[,\s](.|\n)+\(:require[\s,][^)]+?\)", 0)
 		print("require code region: " + str(require_region))
 		if ns_and_the_1st_arg_region.end() < 0:
-			# 2-1. There id no "ns" declaration
+			# 2-1. There id no ns form
 			end_of_shebang_or_dependencies_regions = v.find_all(r"^(#!.+|'\{:dependencies(.|\n)+?\})")
 			if len(end_of_shebang_or_dependencies_regions) > 0:
 				# 2-1-1. There are shebang or dependencies
-				require_code = "\n\n(require '({0} :as {1}))\n".format(ns, ns_alias)
+				require_code = "\n(require '[{0} :as {1}])".format(ns, alias)
 				require_code_insertion_pos = end_of_shebang_or_dependencies_regions[-1].end()
 			else:
 				# 2-1-2. There are not both shebang and dependencies
-				require_code = "(require '({0} :as {1}))\n".format(ns, ns_alias)
+				require_code = "(require '[{0} :as {1}])\n".format(ns, alias)
 				require_code_insertion_pos = 0
 			v.insert(edit, require_code_insertion_pos, require_code)
 			added_require_alias_end_pos = require_code_insertion_pos + len(require_code) - 3
 		elif require_region.end() < 0:
-			# 2-2. There is no ":require" in ns declaration
-			require_code = "\n  (:require [{0} :as {1}])".format(ns, ns_alias)
+			# 2-2. There is no ":require" in ns form
+			require_code = "\n  (:require [{0} :as {1}])".format(ns, alias)
 			require_code_insertion_pos = ns_and_the_1st_arg_region.end()
 			v.insert(edit, require_code_insertion_pos, require_code)
 			added_require_alias_end_pos = require_code_insertion_pos + len(require_code) - 2
 		else:
-			# 2-3. There is ":require" in ns declaration
-			require_code = "\n            [{0} :as {1}]".format(ns, ns_alias)
+			# 2-3. There is ":require" in ns form
+			require_code = "\n            [{0} :as {1}]".format(ns, alias)
 			require_code_insertion_pos = require_region.end() - 1
 			v.insert(edit, require_code_insertion_pos, require_code)
 			added_require_alias_end_pos = require_code_insertion_pos + len(require_code) - 1
-		alias_text_regions.append(sublime.Region(added_require_alias_end_pos - len(ns_alias), added_require_alias_end_pos))
+		replaced_regions.append(sublime.Region(added_require_alias_end_pos - len(alias), added_require_alias_end_pos))
 
-		# 3. replace all of the inline namespace
+		# replace all of the inlines
 		ns_regexp = ns.replace(".", "\.") # TODO: general and safe way to `string -> regexp` conversion
-		inline_ns_text_count = len(v.find_all(ns_regexp)) - 1
-		for i in range(inline_ns_text_count):
-			# TODO: finding in each loop is bad effect for performance.
-			ns_regions = v.find_all(ns_regexp)
-			inline_ns_region = ns_regions[1]
-			v.replace(edit, inline_ns_region, ns_alias)
-			begin = inline_ns_region.begin()
-			alias_text_regions.append(sublime.Region(begin, begin + len(ns_alias)))
+		cnt = len(v.find_all(ns_regexp)) - 1
+		for i in range(cnt):
+			region = v.find_all(ns_regexp)[1]
+			v.replace(edit, region, alias)
+			begin = region.begin()
+			replaced_regions.append(sublime.Region(begin, begin + len(alias)))
 
-		# 4. select all the alias which is replaced
+		# select all the replaced
+		sel = self.view.sel()
 		sel.clear()
-		sel.add_all(alias_text_regions)
-		print("ClojureNsExtractor finished")
+		sel.add_all(replaced_regions)
