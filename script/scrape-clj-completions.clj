fcf0915c837ec4cd66f9c5f922fd721cf8fd0b9f
hata <hata@stylerecipe.co.jp>
2016-07-05 15:06:25 +0900

    Fix #1 duplicated entry in completions.

--- script/scrape-clj-completions.clj
+++ script/scrape-clj-completions.clj
@@ -7,20 +7,64 @@
 (require '[clojure.java.io :as io])
 (require '[clojure.string :as s])
 
-(def url "https://clojuredocs.org/core-library/vars")
+(def indent "    ")
 
-(def namespace-selector [:ul.nav :> :li :> :a html/text-node])
+(defn- fmt [coll]
+  (->> coll
+    (sort-by (comp s/lower-case first))
+    dedupe
+    (group-by (comp s/lower-case first))
+    sort
+    (map #(s/join "\",\"" (last %)))
+    (s/join (str "\",\n" indent "\""))
+    (format (str indent "\"%s\","))))
 
-(def var-selector [:ul.var-list :> :li :> :a html/text-node])
+;; clj
 
-(let [nodes (html/html-resource (io/reader url))]
-  (->> (html/select nodes namespace-selector)
-       (filter #(s/starts-with? % "clojure."))
-       (s/join "\",\"")
-       (printf "\"%s\","))
-  (println "\n")
-  (->> (html/select nodes var-selector)
-       (filter #(< 1 (count %)))
-       (s/join "\",\"")
-       (printf "\"%s\","))
-  (println "\n"))
+(def clj-namespace-selector [:ul.nav :> :li :> :a html/text-node])
+
+(def clj-var-selector [:ul.var-list :> :li :> :a html/text-node])
+
+(defn scrape-clj-completions []
+  (let [nodes (html/html-resource (io/reader "https://clojuredocs.org/core-library/vars"))]
+    (println (str indent "// Namespaces"))
+    (->> (html/select nodes clj-namespace-selector)
+      (filter #(s/starts-with? % "clojure."))
+      fmt
+      println)
+    (println (str indent "// Vars"))
+    (->> (html/select nodes clj-var-selector)
+      (filter #(< 1 (count %)))
+      fmt
+      println)))
+
+;; cljs
+
+(def cljs-namespace-selector [:h4 :> :a html/text-node])
+
+(def cljs-var-selector [:span :> :a])
+
+(defn scrape-cljs-completions []
+  (let [nodes (html/html-resource (io/reader "http://cljs.github.io/api/"))]
+    (println (str indent "// Namespaces"))
+    (->> (html/select nodes cljs-namespace-selector)
+      (filter #(re-matches #"^(cljs\.|clojure\.).*" %))
+      fmt
+      println)
+    (println (str indent "// Vars"))
+    (->> (html/select nodes cljs-var-selector)
+      (map (fn [scraped] {:text (s/join "" (:content scraped))
+                          :href (get-in scraped [:attrs :href])}))
+      (filter #(and (< 1 (count (:text %)))
+                    (re-matches #"^/api/(special|cljs\.|clojure\.).*" (:href %))))
+      (map #(:text %))
+      fmt
+      println)))
+
+;; main 
+
+(condp = (first *command-line-args*)
+  "clj"  (scrape-clj-completions)
+  "cljs" (scrape-cljs-completions)
+  (do (scrape-clj-completions)
+      (scrape-cljs-completions)))
